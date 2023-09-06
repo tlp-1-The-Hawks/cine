8ac9c3a0d829f05124c99b56153db931c8758f86
cespedesBrian <cepedesaguilerabrian@gmail.com>
2023-09-06 16:02:22 -0300

    Se levantó el server y se agregaron las variables de entorno

---
+++ server/app.js
@@ -0,0 +1,19 @@
+// Importacion de bibliotecas
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import {environments} from './config/environments.js';
+
+// Middlewares
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
+app.use(helmet({contentSecurityPolicy: false}));
+
+// Servidor escuchando
+app.listen(environments.PORT, () => {
+  console.log(`Server on http://localhost:${environments.PORT}`);
+});
---
+++ server/config/environments.js
@@ -0,0 +1,13 @@
+import 'dotenv/config';
+
+export const environments = {
+  PORT: process.env.PORT || 4000,
+  DB: {
+    DB_NAME: process.env.DB_NAME,
+    DB_HOST: process.env.DB_HOST,
+    DB_DIALECT: process.env.DB_DIALECT,
+    DB_USER: process.env.DB_USER,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    DB_PORT: process.env.DB_PORT,
+  },
+};
