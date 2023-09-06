adc79acf0921565556acc79ee59372f1b833ebb4
cespedesBrian <cepedesaguilerabrian@gmail.com>
2023-09-06 16:29:43 -0300

    Se agrego el modelado de user

---
+++ server/models/user_model.js
@@ -0,0 +1,33 @@
+import {sequelize} from '../config/database.js';
+import {DataTypes} from 'sequelize';
+
+export const UserModel = sequelize.define(
+  'User',
+  {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
