b54b4dc9177f68540389441efc8102df36515925
Rodri.V <veronrodrigo98@gmail.com>
2023-09-13 15:58:44 -0300

    correccion de bugs

--- server/config/associations.js
+++ server/config/associations.js
@@ -7,67 +7,72 @@ import { MovieModel } from "../models/movie_model.js";
 
 //cinema and booking
 cinemaModel.hasMany(bookingModel, {
-    foreignKey: "cineId",
-    sourceKey: "id"
+  foreignKey: "cineId",
+  sourceKey: "id"
 })
 
 
 bookingModel.belongsTo(cinemaModel, {
-    foreignKey: "cineId",
-    targetKey: "id"
+  foreignKey: "cineId",
+  targetKey: "id"
 })
 
 //user and booking
 UserModel.hasMany(bookingModel, {
-    foreignKey: "userId",
-    sourceKey: "id"
+  foreignKey: "userId",
+  sourceKey: "id"
 })
 
 
 bookingModel.belongsTo(UserModel, {
-    foreignKey: "userId",
-    targetKey: "id"
+  foreignKey: "userId",
+  targetKey: "id"
 })
 
 //user and comments
 UserModel.hasMany(commentModel, {
-    foreignKey: "userId",
-    sourceKey: "id"
+  foreignKey: "userId",
+  sourceKey: "id"
 })
 
 commentModel.belongsTo(UserModel, {
-    foreignKey: "userId",
-    targetKey: "id"
+  foreignKey: "userId",
+  targetKey: "id"
 })
 
 //movie and comments 
 MovieModel.hasMany(commentModel, {
-    foreignKey: "movieId",
-    sourceKey: "id"
+  foreignKey: "movieId",
+  sourceKey: "id"
 })
 
 commentModel.belongsTo(MovieModel, {
-    foreignKey: "movieId",
-    targetKey: "id"
+  foreignKey: "movieId",
+  targetKey: "id"
 })
 
 //booking and movie 
 MovieModel.hasMany(bookingModel, {
-    foreignKey: "movieId",
-    sourceKey: "id"
+  foreignKey: "movieId",
+  sourceKey: "id"
 })
 
 bookingModel.belongsTo(MovieModel, {
-    foreignKey: "movieId",
-    targetKey: "id"
-})
+  foreignKey: 'movieId',
+  targetKey: 'id',
+});
+
+
+MovieModel.belongsToMany(cinemaModel, { through: movieCinema })
+cinemaModel.belongsTo(MovieModel, { through: movieCinema })
+
 
 export async function startDb() {
-    try {
-        await sequelize.sync({ force: false });
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    await sequelize.sync({ force: false });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 
