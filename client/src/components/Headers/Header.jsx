5baeaa944cff0c01beb5fda44a586db4b7f40f4e
Rodri.V <veronrodrigo98@gmail.com>
2023-10-04 16:21:29 -0300

    se solucionaron bugs

--- client/src/components/Headers/Header.jsx
+++ client/src/components/Headers/Header.jsx
@@ -1,22 +1,18 @@
 import React from 'react';
 import '../../../public/style/Header.css';
-<<<<<<<< HEAD:client/src/components/Header/Header.jsx
-import { Login } from './Login.jsx'
-========
-import { Login } from './ButtonLoginRegister.jsx'
->>>>>>>> origin/develop-brian:client/src/components/Headers/Header.jsx
+import { Login } from './ButtonLoginRegister.jsx';
 export const Header = () => {
   return (
     <header>
       <nav className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 border-bottom">
         <div className="col-md-3 mb-2 mb-md-0r d-flex">
           <a href="/">
-          <div className="contimgtitu rounded d-flex">
-            <img src="/img/logo.png" alt="Logo" />
-            <div className="d-flex align-items-center">
-              <p className="titulo">Cines Formosa</p>
+            <div className="contimgtitu rounded d-flex">
+              <img src="/img/logo.png" alt="Logo" />
+              <div className="d-flex align-items-center">
+                <p className="titulo">Cines Formosa</p>
+              </div>
             </div>
-          </div>
           </a>
         </div>
 
