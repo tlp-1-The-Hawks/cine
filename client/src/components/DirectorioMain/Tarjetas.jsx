06bd0840bcb42220828a3768c8eec20a7461b43e
Rodri.V <veronrodrigo98@gmail.com>
2023-10-11 13:36:46 -0300

    se agrego un fetch en el formulario de reserva

--- client/src/components/DirectorioMain/Tarjetas.jsx
+++ client/src/components/DirectorioMain/Tarjetas.jsx
@@ -8,46 +8,44 @@ export const Tarjetas = ({ moviesWithCinemas }) => {
     const cinemaId = e.currentTarget.getAttribute("data-cinema-id");
     const movieId = e.currentTarget.getAttribute("data-movie-id");
 
-    //   const responses = await fetch(`http://localhost:4000/api/movie-cinema/${movieId}/${cinemaId}`,{
-    //     method: "GET"
-    //   })
-    //   console.log(responses)
+    window.location.href = `/reserva?movie=${movieId}&cinema=${cinemaId}`;
+    console.log(cinemaId, movieId);
 
-    // }
+  }
 
-    return (
-      <section className=''>
-        <div className="container">
-          <div className="row d-flex">
-            {moviesWithCinemas.map((movie) => (
-              <div key={movie.id} className="col-md-3 col-sm-12 d-flex justify-content-center">
-                <div className="card">
-                  <img src="/img/image-example.png" className="card-img-top" alt="..." />
-                  <div className="card-body">
-                    <h5 className="card-title">{movie.title}</h5>
-                    {movie.information.map((info) => (
-                      <p className="card-text">Género: {info.genre.genre}</p>
-                    ))}
-                    <p className="card-text">Cines disponibles:</p>
-                    {movie.cinemas.map((cine) => (
-                      <Link
-                        key={cine.id}
-                        to="#"
-                        data-cinema-id={cine.id}
-                        data-movie-id={movie.id}
-                        type='button'
-                        className='btn btn-outline-dark'
-                        onClick={infoMovie}
-                      >
-                        {cine.name}
-                      </Link>
-                    ))}
-                  </div>
+  return (
+    <section className=''>
+      <div className="container">
+        <div className="row d-flex">
+          {moviesWithCinemas.map((movie) => (
+            <div key={movie.id} className="col-md-3 col-sm-12 d-flex justify-content-center">
+              <div className="card">
+                <img src="/img/image-example.png" className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">{movie.title}</h5>
+                  {movie.information.map((info) => (
+                    <p className="card-text">Género: {info.genre.genre}</p>
+                  ))}
+                  <p className="card-text">Cines disponibles:</p>
+                  {movie.cinemas.map((cine) => (
+                    <Link
+                      key={cine.id}
+                      to="#"
+                      data-cinema-id={cine.id}
+                      data-movie-id={movie.id}
+                      type='button'
+                      className='btn btn-outline-dark'
+                      onClick={infoMovie}
+                    >
+                      {cine.name}
+                    </Link>
+                  ))}
                 </div>
               </div>
-            ))}
-          </div>
+            </div>
+          ))}
         </div>
-      </section>
-    );
-  }
+      </div>
+    </section>
+  );
+}
--- client/src/components/Formularios/FormReserva.jsx
+++ client/src/components/Formularios/FormReserva.jsx
@@ -1,31 +1,56 @@
 import '../../assets/style/FormReserva.css'
+import { useState, useEffect } from 'react';
+const urlSearchParams = new URLSearchParams(window.location.search);
+const params = Object.fromEntries(urlSearchParams.entries());
+const { movie, cinema } = params;
+console.log(movie, cinema)
 
 export const FormReserva = () => {
+  const [info, setInfo] = useState({})
+  const [price, setPrice] = useState("Cargando...");
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/api/movies/${movie}/${cinema}`, {
+      method: 'GET'
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setInfo(data)
+        setPrice(data.cinemas[0].information[0].price)
+      })
+      .catch((error) => console.log(error));
+  }, [])
+
+
 
   return (
-    <div className="contenedor">
+    < div className="contenedor" >
+
       <div className="formBoxes">
-        <form name='formreserva'>
+        <form key={info.id} name='formreserva'>
           <h2>Reserva de Asientos de Cine</h2>
 
-        <div className="inputBox">
-          <label>Número de Boletos</label>
-          <input
-            type="number"
-          />
-        </div>
+          <p>{info.hasOwnProperty("cinemas") ? info.cinemas[0].name : ""}</p>
+          <p>{price}</p>
+          <div className="inputBox">
+            <label>Número de Boletos</label>
+            <input
+              type="number"
+            />
+          </div>
 
-        <div className="inputBox">
-          <label>Precio Final</label>
-          <p>(Precio del cine)</p>
-        </div>
-        
-        <div className='boton'>
+          <div className="inputBox">
+            <label>Precio Final</label>
+            <p>(Precio del cine)</p>
+          </div>
+
+          <div className='boton'>
             <input type="submit" className='boton' />
-        </div>
+          </div>
 
-      </form>
+        </form>
       </div>
-    </div>
+
+    </div >
   )
 }
