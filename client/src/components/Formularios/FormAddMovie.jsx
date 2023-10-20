b6725af87fbda22b8b92f459897e450e4d882383
Rodri.V <veronrodrigo98@gmail.com>
2023-10-19 21:15:29 -0300

    se modifico el codigo de la pagina para agregar las peliculas

--- client/src/components/Formularios/FormAddMovie.jsx
+++ client/src/components/Formularios/FormAddMovie.jsx
@@ -23,23 +23,28 @@ export const FormAddMovie = () => {
     const [cinemaId, setCinemaId] = useState(null)
 
     useEffect(() => {
-        fetch('http://localhost:4000/auth/user', {
-            method: 'GET',
-            headers: {
-                'content-type': 'application/json',
-                'authorization': token
-            }
-        })
-            .then((res) => res.json())
-            .then((data) => {
-                if (data.cinemaId === null) {
-                    window.location.href = '/'
-                }
-                else {
-                    return setCinemaId(data.cinemaId)
+        if(token){
+            fetch('http://localhost:4000/auth/user', {
+                method: 'GET',
+                headers: {
+                    'content-type': 'application/json',
+                    'authorization': token
                 }
             })
-            .catch((error) => console.log(error))
+                .then((res) => res.json())
+                .then((data) => {
+                    if (data.cinemaId === null) {
+                        window.location.href = '/'
+                    }
+                    else {
+                        return setCinemaId(data.cinemaId)
+                    }
+                })
+                .catch((error) => console.log(error))
+        }else {
+            window.location.href = '/'
+        }
+
     }, [])
 
 
