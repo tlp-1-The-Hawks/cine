6215d0d7e6fdc5c16a35608ec52c96013a7959c7
Rodri.V <veronrodrigo98@gmail.com>
2023-11-08 20:31:13 -0300

    se agregaron nuevos estilos en el directorio de las peliculas

--- client/src/components/DirectorioMain/Filtros.jsx
+++ client/src/components/DirectorioMain/Filtros.jsx
@@ -4,10 +4,12 @@ import { Tarjetas } from './Tarjetas';
 
 export const Filtros = ({ movies, cinemas }) => {
   const [selectedCinemaId, setSelectedCinemaId] = useState(null);
+  const [cineName, setCineName] = useState('')
 
   const handleFiltro = (e) => {
     const cinemaId = e.target.id === "all" ? null : parseInt(e.target.id);
     setSelectedCinemaId(cinemaId);
+    setCineName(e.target.name)
   }
 
  
@@ -16,38 +18,32 @@ export const Filtros = ({ movies, cinemas }) => {
         return movie.cinemas.some((cinema) => cinema.id === selectedCinemaId);
       })
     : movies;
-
   return (
     <>
       <div className="filtro bg-filtro-container container mb-5">
         <div className="row">
-          <h1 className='text-center'>Cartelera:</h1>
-
+          <h1 className='text-center'>Cartelera: {cineName}</h1>
           <div className="row justify-content-center">
-            <div className="col-sm-4 col-lg-6">
-              <div className="form-group">
-                <div className="row">
+                <div className="col filtrobuttons text-center rounded">
+                     <button  type="button"  className="filtro1 btn w-100 text-white" onClick={handleFiltro} id="all">
+                         TODAS
+                          </button>
+                    </div>
                   <div className='col'>
                     <div className="filtrobuttons dropdawn text-center rounded">
-                      <button  type="button" className="filtro1 w-50 btn text-white" onClick={handleFiltro} id="all">
-                        Todos
-                      </button>
-
-                      <button className="dropdown-toggle filtro2 w-50 btn text-white" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <button className="dropdown-toggle filtro2 btn text-white w-100" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                         SELECCIONAR CINE
                       </button>
+                       
                       <ul className="dropdown-menu">
                         {
                           cinemas.map((cine) => (
-                            <li key={cine.id}><button id={cine.id} onClick={handleFiltro} className='btn w-100'>{cine.name}</button></li>
+                            <li key={cine.id}><button name={cine.name}  id={cine.id} onClick={handleFiltro} className='btn w-100'>{cine.name}</button></li>
                           ))
                         }
                       </ul>
                     </div>
                   </div>
-                </div>
-              </div>
-            </div>
           </div>
         </div>
       </div>
--- client/src/hooks/filtro/FindMovieForCinema.js
+++
@@ -1,7 +0,0 @@
-export async function findMovieForCinema(cinemaId) {
-    const response = await fetch(`http://localhost:4000/api/movies/${cinemaId}`)
-
-    const data = await response.json()
-
-    return data
-}
