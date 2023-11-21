5234768797fb833c239f0f02c54ee43d9d887065
Rodri.V <veronrodrigo98@gmail.com>
2023-11-21 15:18:27 -0300

    se borro codigo innecesario

--- client/src/components/DirectorioMain/AddHall.jsx
+++ client/src/components/DirectorioMain/AddHall.jsx
@@ -13,17 +13,17 @@ export const AddHall = () => {
     })
 
     const handleChange = (e) => {
-        const {name, value} = e.target
+        const { name, value } = e.target
         setFormState({
             ...formState,
-            [name] : value
+            [name]: value
         })
 
         if (e.target.name === "row") {
-            setRowState(e.target.value); 
+            setRowState(e.target.value);
         }
         if (e.target.name === "column") {
-            setColumnState(e.target.value); 
+            setColumnState(e.target.value);
         }
     };
 
@@ -40,18 +40,18 @@ export const AddHall = () => {
             column: j,
             value: e.target.innerText
         };
-    
+
         const buttonIndex = selectedButtons.findIndex(button =>
             button.row === i && button.column === j
         );
-    
+
         if (buttonIndex !== -1) {
-            // Si el botón ya está seleccionado, lo elimina del array
+
             const updatedButtons = [...selectedButtons];
             updatedButtons.splice(buttonIndex, 1);
             setSelectedButtons(updatedButtons);
         } else {
-            // Si el botón no está seleccionado, lo agrega al array
+
             setSelectedButtons(prevSelected => [...prevSelected, buttonInfo]);
 
         }
@@ -59,8 +59,8 @@ export const AddHall = () => {
 
     const generateButtons = () => {
         const buttons = [];
-        let buttonValue = 1; // Valor inicial para los botones
-    
+        let buttonValue = 1;
+
         for (let i = 0; i < rowState; i++) {
             const row = [];
             for (let j = 0; j < columnState; j++) {
@@ -72,23 +72,23 @@ export const AddHall = () => {
                         key={`button-${i}-${j}`}
                         onClick={(e) => selectButton(e, i, j)}
                         className={`seatingButton btn m-1 btn-responsive ${isButtonSelected ? 'btn-danger' : 'btn-dark'}`}
-                 
+
                     >
-                        {buttonValue} {/* Mostrar valor único */}
+                        {buttonValue}
                     </button>
                 );
-                buttonValue++; // Incrementar el valor para el siguiente botón
+                buttonValue++;
             }
-        buttons.push(
-            <div key={`row-${i}`} className="d-flex justify-content-center">
-                {row}
-            </div>
-        );
+            buttons.push(
+                <div key={`row-${i}`} className="d-flex justify-content-center">
+                    {row}
+                </div>
+            );
         }
-    
+
         return buttons;
     };
-    
+
 
     return (
         <div className="container mt-5">
@@ -101,7 +101,7 @@ export const AddHall = () => {
                 </div>
                 <div className="col">
                     <div className="row justify-content-center">
-                        <label  htmlFor="">Capacidad</label>
+                        <label htmlFor="">Capacidad</label>
                         <input onChange={handleChange} value={formState.capacity} name="capacity" className="rounded w-75" type="text" />
                     </div>
                 </div>
@@ -133,7 +133,7 @@ export const AddHall = () => {
                 </div>
             </div>
             <div className="row mt-5 text-center">
-                {/* Renderizar los botones */}
+
                 {generateButtons()}
             </div>
             <AddHallSubmit
@@ -143,8 +143,8 @@ export const AddHall = () => {
                 formState={formState}
                 selectedButtons={selectedButtons}
             />
-            
-            
+
+
         </div>
     );
 };
--- client/src/components/DirectorioMain/EditHall.jsx
+++ client/src/components/DirectorioMain/EditHall.jsx
@@ -3,14 +3,14 @@ import { useEffect, useState } from "react"
 import Swal from "sweetalert2"
 import { CustomFetch } from "../../api/customFetch.js"
 
-export const EditHall = ({halls}) => {
+export const EditHall = ({ halls }) => {
 
     const [hallState, setHallState] = useState([])
     useEffect(() => {
         setHallState(halls)
         console.log(halls)
     }, [halls])
-    
+
     const deleteHall = (e) => {
         e.preventDefault()
 
@@ -18,7 +18,7 @@ export const EditHall = ({halls}) => {
             title: '¿Seguro que quieres eliminar?',
             icon: 'question',
             confirmButtonText: 'Aceptar',
-            showCancelButton: 'cancelar',
+            showCancelButton: 'Cancelar',
             width: '50%',
             padding: '1rem',
             background: '#DBCBCB',
@@ -31,33 +31,33 @@ export const EditHall = ({halls}) => {
                 {
                     hallState.map((hall) => (
                         <div key={hall.id} className="col-12 col-md-6 mt-2 mb-2">
-                                    <div className="container mt-3 border border-white rounded  bg-dark ">
-                                        <div className="row ms-2">
-                                            <div className="col">
-                                                <p className="">Número de sala: {hall.nr_hall}</p>
-                                            </div>
-                                            <div className="col">
-                                                <p className="">Capacidad: {hall.capacity}</p>
-                                            </div>
-                                        </div>
-                                        <div className="row ms-2">
-                                            <div className="col">
-                                                <p className="">Filas: {hall.row}</p>
-                                            </div>
-                                            <div className="col">
-                                                <p className="">Columnas: {hall.column}</p>
-                                            </div>
-                                        </div>
+                            <div className="container mt-3 border border-white rounded  bg-dark ">
+                                <div className="row ms-2">
+                                    <div className="col">
+                                        <p className="">Número de sala: {hall.nr_hall}</p>
                                     </div>
-                                    <div className="d-flex justify-content-end"> 
-                                        <div className="">
-                                            <button className="text-dark requestsButton bg-white btn btn-white ">Editar</button>
-                                        </div>
-                                        <div className="">
-                                            <button 
-                                               id={hall.id} onClick={deleteHall}   className="requestsButton  btn btn-danger">Eliminar</button>
-                                        </div>
+                                    <div className="col">
+                                        <p className="">Capacidad: {hall.capacity}</p>
                                     </div>
+                                </div>
+                                <div className="row ms-2">
+                                    <div className="col">
+                                        <p className="">Filas: {hall.row}</p>
+                                    </div>
+                                    <div className="col">
+                                        <p className="">Columnas: {hall.column}</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="d-flex justify-content-end">
+                                <div className="">
+                                    <button className="text-dark requestsButton bg-white btn btn-white ">Editar</button>
+                                </div>
+                                <div className="">
+                                    <button
+                                        id={hall.id} onClick={deleteHall} className="requestsButton  btn btn-danger">Eliminar</button>
+                                </div>
+                            </div>
                         </div>
                     ))
                 }
